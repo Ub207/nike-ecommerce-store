92fecf7ac2a31c91cac0927395024fd77f90be47
Ub207 <usmaanubaidurrehman@gmail.com>
2024-12-14 22:55:16 +0500

    website done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 
 
@@ -21,7 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
    <Header />
-        {children}</body>
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import GearUp from "@/components/GearUp";
+import { CarouselSize } from "@/components/BEST"
+
 
 export default function Home() {
   return (
-   <div>
   
+  <main>
     <Hero />
     <GearUp />
-   </div>
+   </main>
   );
 }
