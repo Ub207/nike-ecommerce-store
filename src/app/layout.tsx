3bdd1da6699181dcd1a2eed371bc10e08f03ff1d
Ub207 <usmaanubaidurrehman@gmail.com>
2024-12-15 21:48:56 +0500

    website done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 
 
@@ -23,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
    <Header />
         {children}
-        <Footer />
+  
         </body>
     </html>
   );
